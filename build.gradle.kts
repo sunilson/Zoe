377e38e3c4b4e23a3cfc295a02d69ab714ff6233
Linus Weiss <weisslinus@gmail.com>
2020-06-12 13:01:58 +0200

    Add buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    val kotlin_version by extra("1.3.72")
     repositories {
         google()
         jcenter()
@@ -9,7 +10,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
---
+++ buildSrc/src/main/java/at/sunilson/buildsrc/Dependencies.kt
@@ -0,0 +1,5 @@
+package at.sunilson.buildsrc
+
+object Dependencies {
+    const val retrofit = "com.squareup.retrofit2:retrofit:2.9.0"
+}
