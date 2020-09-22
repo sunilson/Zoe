e359fad03eb08d49f18b532511d538be954a6313
Linus Weiss <linus@arkulpa.at>
2020-09-22 16:35:46 +0200

    Schedule workaround

--- chargeSchedule/src/main/java/at/sunilson/chargeSchedule/domain/UpdateChargeSchedule.kt
+++ chargeSchedule/src/main/java/at/sunilson/chargeSchedule/domain/UpdateChargeSchedule.kt
@@ -1,18 +1,13 @@
 package at.sunilson.chargeSchedule.domain
 
 import at.sunilson.chargeSchedule.data.ChargeScheduleService
-import at.sunilson.scheduleCore.data.remote.NetworkScheduleDay
-import at.sunilson.scheduleCore.data.remote.NetworkSchedule
 import at.sunilson.core.usecases.AsyncUseCase
 import at.sunilson.networkingcore.KamereonPostBody
-import at.sunilson.presentationcore.extensions.padZero
 import at.sunilson.scheduleCore.data.createNetworkChargeSchedule
 import at.sunilson.scheduleCore.domain.entities.Schedule
-import at.sunilson.scheduleCore.domain.entities.ScheduleDay
 import at.sunilson.vehiclecore.domain.VehicleCoreRepository
 import com.github.kittinunf.result.coroutines.SuspendableResult
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 internal data class UpdateChargeScheduleParams(
@@ -25,20 +20,28 @@ internal class UpdateChargeSchedule @Inject constructor(
     private val refreshAllChargeSchedules: RefreshAllChargeSchedules,
     private val vehicleCoreRepository: VehicleCoreRepository
 ) : AsyncUseCase<Unit, UpdateChargeScheduleParams>() {
-      override suspend fun run(params: UpdateChargeScheduleParams) =
+    override suspend fun run(params: UpdateChargeScheduleParams) =
         SuspendableResult.of<Unit, Exception> {
-            chargeScheduleService.setChargingSchedule(
-                vehicleCoreRepository.kamereonAccountID,
-                params.vin,
-                KamereonPostBody(
-                    KamereonPostBody.Data(
-                        type = "ChargeSchedule",
-                        attributes = mapOf(
-                            "schedules" to createNetworkChargeSchedule(params.chargeSchedules)
-                        )
+            // As a hacky workaround to the wonky Renault API we try to set and refresh multiple times
+            setChargingSchedule(params)
+            setChargingSchedule(params)
+            refreshAllChargeSchedules(params.vin)
+            delay(1000L)
+            refreshAllChargeSchedules(params.vin)
+        }
+
+    private suspend fun setChargingSchedule(params: UpdateChargeScheduleParams) {
+        chargeScheduleService.setChargingSchedule(
+            vehicleCoreRepository.kamereonAccountID,
+            params.vin,
+            KamereonPostBody(
+                KamereonPostBody.Data(
+                    type = "ChargeSchedule",
+                    attributes = mapOf(
+                        "schedules" to createNetworkChargeSchedule(params.chargeSchedules)
                     )
                 )
             )
-            refreshAllChargeSchedules(params.vin)
-        }
+        )
+    }
 }
