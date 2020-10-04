3ccc700e25e68c8fb6cb694817d0b7bd2579fefb
Linus Weiss <linus@arkulpa.at>
2020-10-04 19:20:16 +0200

    Update spacing

--- chargeStatistics/src/main/java/at/sunilson/chargestatistics/domain/GetMostChargedWeekday.kt
+++ chargeStatistics/src/main/java/at/sunilson/chargestatistics/domain/GetMostChargedWeekday.kt
@@ -24,7 +24,7 @@ class GetMostChargedWeekday @Inject constructor() :
 
             Statistic.Fact(
                 "mostChargedWeekday",
-                "Tag mit meisten Ladungen",
+                "Meist geladen am",
                 "${weekday?.formatPattern("EEEE")}"
             )
         }
