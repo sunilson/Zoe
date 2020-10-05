0890623847dd4e69c8b1321434084dc55466e2e8
Linus Weiss <linus@arkulpa.at>
2020-10-05 21:12:36 +0200

    Adjust day count calculation

--- chargeStatistics/src/main/java/at/sunilson/chargestatistics/domain/GetAverageEnergyPerDay.kt
+++ chargeStatistics/src/main/java/at/sunilson/chargestatistics/domain/GetAverageEnergyPerDay.kt
@@ -4,8 +4,10 @@ import at.sunilson.chargestatistics.domain.entities.Statistic
 import at.sunilson.chargetracking.domain.entities.ChargeTrackingPoint
 import at.sunilson.core.di.NumberFormatModule
 import at.sunilson.core.usecases.AsyncUseCase
+import at.sunilson.ktx.datetime.toZonedDateTime
 import com.github.kittinunf.result.coroutines.SuspendableResult
 import java.text.NumberFormat
+import java.time.temporal.ChronoUnit.DAYS
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -17,13 +19,16 @@ internal class GetAverageEnergyPerDay @Inject constructor(
         SuspendableResult.of<Statistic.Fact?, Exception> {
             val deChargingProcedures = extractDeChargingProcedures(params).get()
             if (deChargingProcedures.isEmpty()) return@of null
-            val count = deChargingProcedures.size
+
+            val startDate = params.first().timestamp.toZonedDateTime().toLocalDate()
+            val lastDate = params.last().timestamp.toZonedDateTime().toLocalDate()
+            val dayCount = DAYS.between(startDate, lastDate) + 1
             val sum = deChargingProcedures.sumBy { it.energyLevelDifference }
 
             Statistic.Fact(
                 "averageEnergyPerDay",
-                "Ø kWh Verbrauch/Tag",
-                "${formatter.format(sum / count.toFloat())} kWh"
+                "Ø Verbrauch/Tag",
+                "${formatter.format(sum / dayCount.toFloat())} kWh"
             )
         }
 }
