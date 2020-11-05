1e9449a5f8dd5b09832a629c3389df34a09bc735
Linus Weiss <linus@arkulpa.at>
2020-11-05 22:01:09 +0100

    Use orange instead of yellow

--- vehicle/src/main/java/at/sunilson/vehicle/presentation/settingsDialog/ThemeListItemModel.kt
+++ vehicle/src/main/java/at/sunilson/vehicle/presentation/settingsDialog/ThemeListItemModel.kt
@@ -24,14 +24,14 @@ abstract class ThemeListItemModel : EpoxyModelWithHolder<ThemeListItemModel.Hold
     override fun bind(holder: Holder) = holder.run {
         red.setOnClickListener { onThemeChosen(R.style.Base_Theme_Zoe_Red) }
         blue.setOnClickListener { onThemeChosen(R.style.Base_Theme_Zoe_Blue) }
-        yellow.setOnClickListener { onThemeChosen(R.style.Base_Theme_Zoe_Yellow) }
+        orange.setOnClickListener { onThemeChosen(R.style.Base_Theme_Zoe_Orange) }
         green.setOnClickListener { onThemeChosen(R.style.Base_Theme_Zoe_Green) }
     }
 
     class Holder : KotlinEpoxyHolder() {
         val red by bind<MaterialCardView>(R.id.red)
         val blue by bind<MaterialCardView>(R.id.blue)
-        val yellow by bind<MaterialCardView>(R.id.yellow)
+        val orange by bind<MaterialCardView>(R.id.orange)
         val green by bind<MaterialCardView>(R.id.green)
     }
 }
