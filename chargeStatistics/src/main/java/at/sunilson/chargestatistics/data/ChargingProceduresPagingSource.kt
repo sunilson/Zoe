75f17e7a198b1591de0abdbd8eeac972e1f35a39
Linus Weiss <linus@arkulpa.at>
2021-03-08 20:43:02 +0100

    Add timber log to paging source

--- chargeStatistics/src/main/java/at/sunilson/chargestatistics/data/ChargingProceduresPagingSource.kt
+++ chargeStatistics/src/main/java/at/sunilson/chargestatistics/data/ChargingProceduresPagingSource.kt
@@ -7,6 +7,7 @@ import at.sunilson.chargetracking.domain.GetOffsetChargeTrackingPoints
 import at.sunilson.chargetracking.domain.GetOffsetChargeTrackingPointsParams
 import at.sunilson.vehiclecore.domain.VehicleCoreRepository
 import kotlinx.coroutines.flow.firstOrNull
+import timber.log.Timber
 import javax.inject.Inject
 
 internal class ChargingProceduresPagingSource @Inject constructor(
@@ -15,7 +16,7 @@ internal class ChargingProceduresPagingSource @Inject constructor(
     private val vehicleCoreRepository: VehicleCoreRepository
 ) : PagingSource<Int, ChargingProcedure>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ChargingProcedure> {
-        try {
+        return try {
             val offset = params.key ?: 0
             val vin =
                 vehicleCoreRepository.selectedVehicle.firstOrNull() ?: error("No vehicle selected!")
@@ -27,13 +28,14 @@ internal class ChargingProceduresPagingSource @Inject constructor(
                 )
             ).get()
             val chargingProcedures = extractChargingProcedures(chargeTrackingPoints).get()
-            return LoadResult.Page(
+            LoadResult.Page(
                 data = chargingProcedures,
                 null,
                 if (chargeTrackingPoints.isEmpty()) null else offset + chargeTrackingPoints.size
             )
         } catch (error: Exception) {
-            TODO()
+            Timber.e(error)
+            LoadResult.Error(error)
         }
     }
 
